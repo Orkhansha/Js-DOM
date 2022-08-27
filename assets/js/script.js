4d6ef38a190ec96965656da94df87544680070e6
Orkhansha <orkhansha@code.edu.az>
2022-08-27 16:08:54 +0400

    Added space bag's fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,48 +10,71 @@ let text1 = document.querySelector(".input-one");
 
 let text2 = document.querySelector(".input-two");
 
-let resultNum = document.querySelector(".resulte h3")
+let resultNum = document.querySelector(".resulte h3");
 
-function sum (){
-    resultNum.innerText = (parseInt(text1.value) + parseInt(text2.value));
-    alert("***Ededler toplanir***")
+function sum() {
+  if (text1.value == "" | text2.value == "") {
+    alert("***Boshluq buraxmaq olmaz***")
+    return;
+  } else {
+    resultNum.innerText = parseInt(text1.value) + parseInt(text2.value);
+    alert("***Ededler toplanir***");
     text1.value = "";
     text2.value = "";
+  }
 }
 
-function sub (){
-    resultNum.innerText = (parseInt(text1.value) - parseInt(text2.value));
-    alert("***Ededler chixilir***")
-    text1.value = "";
-    text2.value = "";
-}
-
-function mult (){
-    resultNum.innerText = (parseInt(text1.value) * parseInt(text2.value));
-    alert("***Ededler vurulur***")
-    text1.value = "";
-    text2.value = "";
-}
-
-function div (){
-    if (text2.value != 0) {
-        resultNum.innerText = (parseInt(text1.value) / parseInt(text2.value));
-        alert("***Ededler bolunur***")
+function sub() {
+    if (text1.value == "" | text2.value == "") {
+        alert("***Boshluq buraxmaq olmaz***")
+        return;
+      } else {
+        resultNum.innerText = parseInt(text1.value) - parseInt(text2.value);
+        alert("***Ededler chixilir***");
         text1.value = "";
         text2.value = "";
-    }else{
-        alert("Duzgun reqem daxil edin")
-        resultNum.innerText = ""
-    }
+      }
+  
 }
 
-sumBtn.addEventListener("click", sum)
+function mult() {
+    if (text1.value == "" | text2.value == "") {
+        alert("***Boshluq buraxmaq olmaz***")
+        return;
+      } else {
+        resultNum.innerText = parseInt(text1.value) * parseInt(text2.value);
+        alert("***Ededler vurulur***");
+        text1.value = "";
+        text2.value = "";
+      }
+  
+ 
+}
 
+function div() {
+    if (text1.value == "" | text2.value == "") {
+        alert("***Boshluq buraxmaq olmaz***")
+        return;
+      } else {
+        if (text2.value != 0) {
+            resultNum.innerText = parseInt(text1.value) / parseInt(text2.value);
+            alert("***Ededler bolunur***");
+            text1.value = "";
+            text2.value = "";
+          } else {
+            alert("Duzgun reqem daxil edin");
+            resultNum.innerText = "";
+          }
+      }
+  
 
-subBtn.addEventListener("click", sub)
+  
+}
 
+sumBtn.addEventListener("click", sum);
 
-multBtn.addEventListener("click", mult)
+subBtn.addEventListener("click", sub);
 
+multBtn.addEventListener("click", mult);
 
-divBtn.addEventListener("click", div)
+divBtn.addEventListener("click", div);
